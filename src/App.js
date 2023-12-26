b75f9b7b2772a165cf5f8d1653f636314d4865a9
Mustafa Tayyar <mtayyar77@gmail.com>
2023-12-26 20:48:33 +0300

    project created
    project created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage/Homepage"
+import Products from "./pages/Products/Products"
+import Navbar from "./components/Navbar/Navbar"
 
-function App() {
 
-  //mustafa
-  return (
-    <div className="App">
-        merhaba
+// JSX => HTML + JS
+// HTML'de keywordün ismini değiştirmek
+// class => className
+export default function App() {
+  return <>
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="*" element={<div>Not found</div>}></Route>
+      </Routes>
+    </BrowserRouter>
+  </>
 
-    </div>
-  );
 }
-
-export default App;
